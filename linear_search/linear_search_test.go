ad348c731b9fd92043b9bb4e6f89bd90c8d82e6d
Curtis Hammons <chammons@boingo.com>
2023-09-09 11:55:32 -0700

    corrected name

--- linear_search/linear_search_test.go
+++ linear_search/linear_search_test.go
@@ -8,11 +8,11 @@ func TestLinearSearch(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5}
 	target := 3
 	if !linearSearch(arr, target) {
-		t.Errorf("linear_search(%v, %d) should be true", arr, target)
+		t.Errorf("linearSearch(%v, %d) should be true", arr, target)
 	}
 
 	target = 6
 	if linearSearch(arr, target) {
-		t.Errorf("linear_search(%v, %d) should be false", arr, target)
+		t.Errorf("linearSearch(%v, %d) should be false", arr, target)
 	}
 }
