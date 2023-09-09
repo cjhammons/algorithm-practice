957be51aea4a5399f0b5ecacb4f9529f312170c6
Curtis Hammons <chammons@boingo.com>
2023-09-09 13:26:44 -0700

    removed crystal ball

--- search/two_crystal_ball_problem/crystall_ball_test.go
+++ search/two_crystal_ball_problem/crystall_ball_test.go
@@ -9,4 +9,21 @@ func TestTwoCrystalBallHappy(t *testing.T) {
 	if twoCrystalBall(arr) != 6 {
 		t.Errorf("Did not find where balls break")
 	}
+
+	arr = []bool{false, true, true, true, true}
+	if twoCrystalBall(arr) != 1 {
+		t.Errorf("Did not find where balls break")
+	}
+
+	arr = []bool{false, false, false, false, true}
+	if twoCrystalBall(arr) != 5 {
+		t.Errorf("Did not find where balls break")
+	}
+}
+
+func TestTwoCrystalBallSad(t *testing.T) {
+	arr := []bool{false, false, false, false, false}
+	if twoCrystalBall(arr) != -1 {
+		t.Errorf("Found a breaking point that should not exist")
+	}
 }
