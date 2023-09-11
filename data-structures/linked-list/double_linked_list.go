c3c742cb16cbf862e212aedf62c176f4ff3cf12e
Curtis Hammons <chammons@boingo.com>
2023-09-11 08:15:43 -0700

    implemented linked list

--- data-structures/linked-list/double_linked_list.go
+++ data-structures/linked-list/double_linked_list.go
@@ -12,29 +12,103 @@ type DoublyLinkedList struct {
 }
 
 func (l *DoublyLinkedList) Append(value int) {
+	newNode := &Node{Value: value}
+
+	if l.Head == nil {
+		l.Head = newNode
+		l.Tail = newNode
+		return
+	}
+
+	l.Tail.Next = newNode
+	newNode.Prev = l.Tail
+	l.Tail = newNode
 
 }
 
 func (l *DoublyLinkedList) Prepend(value int) {
+	newNode := &Node{Value: value}
 
+	if l.Head == nil {
+		l.Head = newNode
+		l.Tail = newNode
+		return
+	}
+
+	l.Head.Prev = newNode
+	newNode.Next = l.Head
+	l.Head = newNode
 }
 
 func (l *DoublyLinkedList) Delete(value int) {
+	current := l.Head
+	for current != nil {
+		if current.Value == value {
+			if current.Prev != nil {
+				current.Prev.Next = current.Next
+			} else {
+				l.Head = current.Next
+			}
 
+			if current.Next != nil {
+				current.Next.Prev = current.Prev
+			} else {
+				l.Tail = current.Prev
+			}
+
+			return
+		}
+		current = current.Next
+	}
 }
 
 func (l *DoublyLinkedList) length() int {
-	return 0
+	count := 0
+	current := l.Head
+	for current != nil {
+		count++
+		current = current.Next
+	}
+	return count
 }
 
 func (l *DoublyLinkedList) InsertAt(val int, index int) {
+	i := 0
+	n := l.Head
+	for i != index {
+		if n.Next == nil {
+			return
+		}
+		n = n.Next
+		i++
+	}
+	newNode := &Node{Value: val}
 
+	n.Prev.Next = newNode
+	newNode.Next = n
+	newNode.Prev = n.Prev
 }
 
 func (l *DoublyLinkedList) DeleteAt(index int) {
+	i := 0
+	current := l.Head
+	for current != nil {
+		if index == i {
+			if current.Prev != nil {
+				current.Prev.Next = current.Next
+			} else {
+				l.Head = current.Next
+			}
 
-}
+			if current.Next != nil {
+				current.Next.Prev = current.Prev
+			} else {
+				l.Tail = current.Prev
+			}
 
-func (l *DoublyLinkedList) Insert(item int) {
-
+			return
+		}
+		current = current.Next
+		i++
+	}
 }
